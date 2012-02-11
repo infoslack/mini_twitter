44914e305276d70d91581d8679367ad3bfe482c2
Daniel Romero <infoslack@gmail.com>
2012-02-11 11:50:32 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniTwitter::Application.config.secret_token = 'ea1b990de3822f6951a4f5d4a2e8b216d33a7ca4893756e0cf0bd706e6ca76802d45c6dfd28e57c5ce0d498bf9141f25af1ca3642b817c6170929401ec3a8d17'
