f4b92c6a72d5aa6049a9c49d3eaa30defc7937ac
Daniel Romero <infoslack@gmail.com>
2012-02-11 18:38:36 -0300

    Refactory helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def logo
+   logo = image_tag("logo.png", alt: "Mini Twitter", class: "round")
+  end
+
   def full_title(page_title)
     base_title = "Mini Twitter demo app"
     if page_title.empty?
