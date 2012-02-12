c38cf34cc3ca26b3e161085a0740f95db6c9f641
Daniel Romero <infoslack@gmail.com>
2012-02-12 02:05:19 -0300

    Modified size gravatar and implemented tests for validation

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -22,12 +22,29 @@ describe "User Pages" do
       end
     end
 
+    describe "error_messages" do
+      before { click_button "Sign up" }
+
+      let(:error) { 'errors prohibited this user from being saved' }
+
+      it { should have_selector('title', text: 'Sign up') }
+      it { should have_content(error) }
+    end
+
     describe "with valid information" do
       before do
         fill_in "Name",           with: "User Teste"
         fill_in "Email",          with: "teste@domain.com"
         fill_in "Password",       with: "teste123"
         fill_in "Confirmation",   with: "teste123"
+      end
+
+      describe "after saving the user" do
+        before { click_button "Sign up" }
+        let(:user) { User.find_by_email('teste@domain.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.flash.success', text: 'Welcome') }
       end
 
       it "should create a user" do
